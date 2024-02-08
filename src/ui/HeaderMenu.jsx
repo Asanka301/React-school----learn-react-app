f4dbf86d178b407a008b78f1c7e97553791f20ad
Asanka301 <asankagamage301@gmail.com>
2024-02-08 18:48:55 +0530

    Edit hooks

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 //import Logout from "../features/authentication/Logout";
-import ButtonIcon from "./ButtonIcon";
-import { HiOutlineUser } from "react-icons/hi2";
-import { useNavigate } from "react-router-dom";
+
 import DarkModeToggle from "./DarkModeToggle";
 
 const StyledHeaderMenu = styled.ul`
@@ -11,8 +9,6 @@ const StyledHeaderMenu = styled.ul`
 `;
 
 function HeaderMenu() {
-  const navigate = useNavigate();
-
   return (
     <StyledHeaderMenu>
       {/* <li>
